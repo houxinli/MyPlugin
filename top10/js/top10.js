df9b887ed99879fe2895827c706251f5a1462cf9
houxinli <houxinli@hotmail.com>
2018-11-11 21:02:07 +0800

    update

--- top10/js/top10.js
+++ top10/js/top10.js
@@ -2,8 +2,13 @@ $(document).ready(function(){
 	setTimeout(function(){
 		$("div.mainPageListContent1:first .mainPageListTitle a")
 		.each(function(index,e){
+			// e.addClass("Top 10");
+			var url = e.getAttribute("href");
 			e.click();
-		})
+		});
+	}, 1000);
+// TODO: https://www.cnblogs.com/champagne/p/4826611.html 
+// https://www.cnblogs.com/h2zZhou/p/7250076.html
+// local/sync storage
 
-	}, 4000)
 });
