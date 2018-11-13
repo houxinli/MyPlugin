6f2afba718dddc190908e61f06854135325464a6
houxinli <houxinli@hotmail.com>
2018-11-14 01:08:40 +0800

    localStorage Naive version

--- top10/js/top10.js
+++ top10/js/top10.js
@@ -3,8 +3,19 @@ $(document).ready(function(){
 		$("div.mainPageListContent1:first .mainPageListTitle a")
 		.each(function(index,e){
 			// e.addClass("Top 10");
-			var url = e.getAttribute("href");
-			e.click();
+			// localStorage.curT = Date.now();
+			var url = e.getAttribute("href").split('/')[2];
+			console.log(localStorage[url])
+			if(typeof(localStorage[url]) == 'undefined'){
+				e.click();
+				var title = e.innerHTML;
+				localStorage[url] = title;
+				console.log("title: ", title);
+				console.log("saved title: ", localStorage[url])
+			}
+			// console.log("url: ", url);
+			// var post = {url: title};
+
 		});
 	}, 1000);
 // TODO: https://www.cnblogs.com/champagne/p/4826611.html 
