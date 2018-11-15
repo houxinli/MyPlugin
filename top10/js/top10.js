b8306237007b5a8764e822f9dbef33212cf1af0e
houxinli <houxinli@hotmail.com>
2018-11-15 23:50:22 +0800

    Update top10/js/top10.js

--- top10/js/top10.js
+++ top10/js/top10.js
@@ -4,22 +4,40 @@ $(document).ready(function(){
 		.each(function(index,e){
 			// e.addClass("Top 10");
 			// localStorage.curT = Date.now();
-			var url = e.getAttribute("href").split('/')[2];
-			console.log(localStorage[url])
-			if(typeof(localStorage[url]) == 'undefined'){
-				e.click();
-				var title = e.innerHTML;
-				localStorage[url] = title;
-				console.log("title: ", title);
-				console.log("saved title: ", localStorage[url])
+			var id = e.getAttribute("href").split('/')[2];
+			var title = e.innerHTML;
+			console.log("id", id)
+			console.log("title: ", title);
+			if(typeof(localStorage['top10ViewHistory']) == 'undefined'){
+				var obj = new Object();
+				// obj.account TODO
+				obj.num = 1;
+				obj.posts = new Array(1);
+				var post = {'id': id, 'title': title};
+				obj.posts[0] = post;
+				console.log("obj: ", obj)
+				localStorage['top10ViewHistory'] = obj;
+			}else{
+				var obj = localStorage['top10ViewHistory'];
+				console.log("obj: ", obj)
+				var posts = obj.posts;
+				var flag = 0;
+				for (var i = posts.length - 1; i >= 0; i--) {
+					if(posts[i].id == id){
+						flag = 1;
+						break;
+					}
+				}
+				if(flag == 0){
+					obj.num++;
+					var post = {'id': id, 'title': title};
+					obj.posts.push(post);
+					localStorage['top10ViewHistory'] = obj;
+				}
 			}
-			// console.log("url: ", url);
-			// var post = {url: title};
-
+			console.log('localStorage', localStorage);
 		});
 	}, 1000);
-// TODO: https://www.cnblogs.com/champagne/p/4826611.html 
-// https://www.cnblogs.com/h2zZhou/p/7250076.html
-// local/sync storage
-
+// TODO: account storage
+// 从storage中取回帖数等指标判断是否再次打开
 });
