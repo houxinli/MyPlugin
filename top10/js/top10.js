b4244add98fc1384d8a47b3370e286082dfc090c
houxinli <houxinli@hotmail.com>
2018-11-17 12:02:55 +0800

    Update top10.js

--- top10/js/top10.js
+++ top10/js/top10.js
@@ -4,31 +4,35 @@ $(document).ready(function(){
 	setTimeout(function(){
 		$("div.mainPageListContent1:first .mainPageListTitle a")
 		.each(function(index,e){
+			// var url ="www.cc98.org" + e.getAttribute("href");
+			var url =e.getAttribute("href");
 			var id = e.getAttribute("href").split('/')[2];
 			var title = e.innerHTML;
 			console.log("id", id)
 			console.log("title: ", title);
 			if(typeof(localStorage['top10ViewHistory']) == "undefined"){
 				var obj = new Object();
-				// console.log("branch1");
-				// obj.account TODO
 				obj.num = 1;
 				obj[id] = title;
-				// console.log("obj: ", obj);
 				var str = JSON.stringify(obj);
 				localStorage['top10ViewHistory'] = str;
-				// console.log('localStorage', localStorage);
 			}else{
-				// console.log("branch2");
 				var str = localStorage['top10ViewHistory'];
 				obj = JSON.parse(str);
-				// console.log("obj: ", obj)
 				if(obj[id] === undefined){
 					obj[id] = title;
 					obj.num++;
 					e.click();
 					str = JSON.stringify(obj);
 					localStorage['top10ViewHistory'] = str;
+					$.get(url, function(data){
+						console.log("data", data);
+					});
+				}
+				else{
+					$.get(url, function(data){
+						console.log("read:", data);
+					});	
 				}
 			}
 		});
@@ -36,4 +40,5 @@ $(document).ready(function(){
 // TODO: account storage
 // curT = Date.now();
 // 从storage中取回帖数等指标判断是否再次打开
+// 爬取热门回帖数
 });
